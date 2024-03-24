67509c29f724e378dafc0ffe68524d7a7210b7e0
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-03-25 00:34:16 +0400

    [development/refactor] Refactor main.py to improve readability and maintainability
    - Reorganized imports for better readability
    - Moved constants import to the end of imports
    - Created a function make_hash_groups_dir to generate a directory for hash groups
    - Changed the data structure for possible_hashes to a dictionary to store hash values for each algorithm
    - Added logic to create a directory for hash groups and write hash values to respective algorithm files
    - Improved output formatting for possible algorithms
    - Minor code formatting changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
-import constants
-from tabulate import tabulate
+import os
+from datetime import datetime
 from algorithms.identifiers.md5 import MD5
+from tabulate import tabulate
+import constants
 
 parser = argparse.ArgumentParser(description="Identify hashes from a wordlist that integrates with HashCat")
 parser.add_argument('hash_list_file', help='path to the file containing hashes to identify')
@@ -10,8 +12,15 @@ parser.add_argument('hash_list_file', help='path to the file containing hashes t
 def hash_list_parser(hash_list_file_path: str):
     with open(hash_list_file_path, 'r') as f:
         hash_list = f.read().splitlines()
-
     return hash_list
+
+
+def make_hash_groups_dir():
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    hash_groups_dirname = f"hashes-{now}"
+    os.makedirs(hash_groups_dirname, exist_ok=True)
+
+    return hash_groups_dirname
 
 
 if __name__ == '__main__':
@@ -24,15 +33,27 @@ if __name__ == '__main__':
         MD5
     ]
 
+    possible_hashes = {}
+
     for hash_value in hash_list_parser(args.hash_list_file):
-        possible_hashes = []
+        validated_algorithms = []
 
         for algorithm in algorithms:
-            algorithm_instance = algorithm(hash_value)
-            if (algorithm_instance.validate()):
-                possible_hashes.append([f"[+] {algorithm.__name__}"])
+            instance = algorithm(hash_value)
+            if instance.validate():
+                possible_hashes.setdefault(algorithm.__name__, []).append(hash_value)
+                validated_algorithms.append(algorithm.__name__)
 
-        if possible_hashes:
-            print("\n\n", tabulate(possible_hashes, headers=[f"[+] Possible algorithms for {hash_value}"]))
+        if validated_algorithms:
+            print("\n\n", tabulate([
+                [f"[+] {algorithm_name}"] for algorithm_name in validated_algorithms
+            ], headers=[f"Possible algorithms for {hash_value}"]))
+
+    if possible_hashes:
+        hash_group_dir_name = make_hash_groups_dir()
+
+        for hash_group, hashes in possible_hashes.items():
+            with open(os.path.join(hash_group_dir_name, f"{hash_group.lower()}.txt"), "a") as hash_group_file:
+                hash_group_file.writelines(f"{hash}\n" for hash in hashes)
 
     print(constants.GOOD_LUCK)

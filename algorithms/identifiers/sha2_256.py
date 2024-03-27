bd0b937addf73eb8201dc88339b38bf18511cd19
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-03-27 23:20:37 +0400

    Refactor identifier classes to simplify validation
    Removed redundant checks for digit, alpha, and alphanumeric characters in the validate method of MD4, MD5, NTLM, SHA1, and SHA2256 identifier classes. These checks were unnecessary as the is_hex and is_valid_length checks already covered the necessary validations. This refactor improves code readability and maintainability.

--- algorithms/identifiers/sha2_256.py
+++ algorithms/identifiers/sha2_256.py
@@ -13,8 +13,5 @@ class SHA2256(Identifier, HasValidLength):
     def validate(self):
         is_hex = is_hex_str(self.hash_value)
         is_valid_length = len(self.hash_value) == self.get_valid_length()
-        is_digit = self.hash_value.isdigit()
-        is_alpha = self.hash_value.isalpha()
-        is_alphanum = self.hash_value.isalnum()
 
-        return is_hex and is_valid_length and is_alphanum and not is_digit and not is_alpha
+        return is_hex and is_valid_length

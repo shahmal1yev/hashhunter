5dae9676fd9a977eed7e7e9e5259c26e68801b92
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-03-27 22:21:00 +0400

    Use is_hex_str helper function to validate hash values as hexadecimal strings
    Updated the validate method in MD4, MD5, NTLM, SHA1, and SHA2_256 identifier classes to use the is_hex_str helper function to ensure that hash values are hexadecimal strings before performing further validation. This change helps maintain consistency and readability in the code.

--- algorithms/identifiers/md5.py
+++ algorithms/identifiers/md5.py
@@ -1,4 +1,5 @@
 from algorithms.interfaces.identifier import Identifier
+from helpers import is_hex_str
 
 
 class MD5(Identifier):
@@ -9,9 +10,10 @@ class MD5(Identifier):
         self.hash_value = hash_value
 
     def validate(self) -> bool:
+        is_hex = is_hex_str(self.hash_value)
         is_valid_length = len(self.hash_value) == self.BYTE_LENGTH
         is_digit = self.hash_value.isdigit()
         is_alpha = self.hash_value.isalpha()
         is_alphanum = self.hash_value.isalnum()
 
-        return is_valid_length and is_alphanum and not is_digit and not is_alpha
+        return is_hex and is_valid_length and is_alphanum and not is_digit and not is_alpha

07b7d5f9901f13d0b9be26c997d1595f38469a31
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-03-28 00:51:23 +0400

    [development/update] Add new cryptographic hash algorithm classes
    This commit introduces new classes to the `algorithms.identifiers` module, each representing a different cryptographic hash algorithm. The added classes are:
    
    - `SHA2224`: SHA-224 algorithm with a byte size of 28.
    - `SHA2384`: SHA-384 algorithm with a byte size of 48.
    - `SHA3224`: SHA3-224 algorithm with a byte size of 28.
    - `SHA3256`: SHA3-256 algorithm with a byte size of 32.
    - `SHA3384`: SHA3-384 algorithm with a byte size of 48.
    - `SHA3512`: SHA3-512 algorithm with a byte size of 64.
    - `RIPEMD160`: RIPEMD160 algorithm with a byte size of 20.
    - `BLAKE2B512`: BLAKE2b-512 algorithm with a byte size of 64.
    - `GOSTR34112012256`: GOST R 34.11-2012 (Streebog) 256-bit, big-endian algorithm with a byte size of 32.
    - `GOSTR34112012512`: GOST R 34.11-2012 (Streebog) 512-bit, big-endian algorithm with a byte size of 64.
    - `HalfMD5`: Half MD5 algorithm with a byte size of 8.
    - `Keccak224`: Keccak-224 algorithm with a byte size of 28.
    - `Keccak256`: Keccak-256 algorithm with a byte size of 32.
    - `Keccak384`: Keccak-384 algorithm with a byte size of 48.
    - `Keccak512`: Keccak-512 algorithm with a byte size of 64.
    - `Whirlpool`: Whirlpool algorithm with a byte size of 64.
    
    Each class defines the algorithm name and byte size as required attributes.

--- algorithms/identifiers.py
+++ algorithms/identifiers.py
@@ -29,3 +29,83 @@ class SHA2256(Identifier):
 class SHA2512(Identifier):
     algorithm_name: str = "SHA2-512"
     byte_size: int = 64
+
+
+class SHA2224(Identifier):
+    algorithm_name: str = "SHA2-224"
+    byte_size: int = 28
+
+
+class SHA2384(Identifier):
+    algorithm_name: str = "SHA2-384"
+    byte_size: int = 48
+
+
+class SHA3224(Identifier):
+    algorithm_name: str = "SHA3-324"
+    byte_size: int = 28
+
+
+class SHA3256(Identifier):
+    algorithm_name: str = "SHA3-256"
+    byte_size: int = 32
+
+
+class SHA3384(Identifier):
+    algorithm_name: str = "SHA3-384"
+    byte_size: int = 48
+
+
+class SHA3512(Identifier):
+    algorithm_name: str = "SHA3-512"
+    byte_size: int = 64
+
+
+class RIPEMD160(Identifier):
+    algorithm_name: str = "RIPEMD160"
+    byte_size: int = 20
+
+
+class BLAKE2B512(Identifier):
+    algorithm_name: str = "BLAKE2b-512"
+    byte_size: int = 64
+
+
+class GOSTR34112012256(Identifier):
+    algorithm_name: str = "GOST R 34.11-2012 (Streebog) 256-bit, big-endian"
+    byte_size: int = 32
+
+
+class GOSTR34112012512(Identifier):
+    algorithm_name: str = "GOST R 34.11-2012 (Streebog) 512-bit, big-endian"
+    byte_size: int = 64
+
+
+class HalfMD5(Identifier):
+    algorithm_name: str = "Half MD5"
+    byte_size: int = 8
+
+
+class Keccak224(Identifier):
+    algorithm_name: str = "Keccak-224"
+    byte_size: int = 28
+
+
+class Keccak256(Identifier):
+    algorithm_name: str = "Keccak-224"
+    byte_size: int = 32
+
+
+class Keccak384(Identifier):
+    algorithm_name: str = "Keccak-224"
+    byte_size: int = 48
+
+
+class Keccak512(Identifier):
+    algorithm_name: str = "Keccak-224"
+    byte_size: int = 64
+
+
+class Whirlpool(Identifier):
+    algorithm_name = "Whirlpool"
+    byte_size: int = 64
